42dead7de96cab53940678804a57d8a9c4be92f6
Filipi Carlos Rodrigues <fillipicarlos1@gmail.com>
2023-10-23 17:08:35 -0300

    printanto o tabuleiro na tela

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,22 @@ package boardgame;
 
 public class Piece {
 
+	/*não quero que essa posição seja visivel na camada de xadrez */
+	protected Position position; 
+	private Board board;
+	
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+
+	protected Board getBoard() {
+		return board;
+	}
+
+
+	
+	
 }
